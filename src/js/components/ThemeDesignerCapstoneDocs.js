586ae4d62d97f4b7696435f143bf801c680889fb
Bryan Jacquot <jacquot@hpe.com>
2016-03-12 16:53:05 -0700

    Updated with functional brick components.

--- src/js/components/ThemeDesignerCapstoneDocs.js
+++ src/js/components/ThemeDesignerCapstoneDocs.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Gravatar from 'react-gravatar';
 import App from 'grommet/components/App';
 import Header from 'grommet/components/Header';
 import Title from 'grommet/components/Title';
@@ -7,6 +8,8 @@ import Box from 'grommet/components/Box';
 import Brick from 'grommet/components/Brick';
 import Bricks from 'grommet/components/Bricks';
 import Footer from 'grommet/components/Footer';
+import PdfIcon from 'grommet/components/icons/base/DocumentPdf';
+import CursorIcon from 'grommet/components/icons/base/Cursor';
 
 export default class ThemeDesignerCapstoneDocs extends Component {
 
@@ -22,14 +25,34 @@ export default class ThemeDesignerCapstoneDocs extends Component {
           <Header direction="row" justify="between" large={true}
             pad={{horizontal: 'large'}}>
             <Title>Human-Computer Interaction Capstone Project</Title>
+            <Box direction="row">
+              <Title>Bryan Jacquot</Title>
+              <Gravatar email={'jacquot@hpe.com'} />
+            </Box>
           </Header>
-          <Box pad="large">
+          <Box pad={{horizontal:'large'}}>
             <Bricks>
-              <Brick label="Prototype" href="https://invis.io/ZG6CFJX69" colorIndex="neutral-3" type="large" />
-              <Brick label="Proposal" href="./docs/M1-BryanJacquot.pdf" colorIndex="neutral-1" texture="img/m1-texture.png"/>
-              <Brick label="User Needs" href="./docs/Ms-BryanJacquot.pdf" colorIndex="neutral-2" />
-              <Brick label="Design" href="./docs/Ms-BryanJacquot.pdf" colorIndex="accent-1" />
-              <Brick label="Evaluation" href="./docs/Ms-BryanJacquot.pdf" colorIndex="neutral-4" />
+              <Brick label="Interactive Prototype" href="https://invis.io/ZG6CFJX69" colorIndex="brand" type="wide">
+                <CursorIcon size="large" colorIndex="light-1" />
+              </Brick>
+              <Brick label="Proposal" href="https://github.com/bryanjacquot/theme-designer-capstone/blob/master/docs/M1-JacquotBryan.pdf" colorIndex="neutral-1">
+                <PdfIcon size="large" colorIndex="light-1" />
+              </Brick>
+              <Brick label="User Needs" href="https://github.com/bryanjacquot/theme-designer-capstone/blob/master/docs/M2-JacquotBryan.pdf" colorIndex="neutral-2">
+                <PdfIcon size="large" colorIndex="light-1" />
+              </Brick>
+              <Brick label="Design" href="https://github.com/bryanjacquot/theme-designer-capstone/blob/master/docs/M3-JacquotBryan.pdf" colorIndex="accent-1">
+                <PdfIcon size="large" colorIndex="light-1" />
+              </Brick>
+              <Brick label="Prototype Description" href="https://github.com/bryanjacquot/theme-designer-capstone/blob/master/docs/M4-JacquotBryan.pdf" colorIndex="grey-3">
+                <PdfIcon size="large" colorIndex="light-1" />
+              </Brick>
+              <Brick label="Evaluation" href="https://github.com/bryanjacquot/theme-designer-capstone/blob/master/docs/M5-JacquotBryan.pdf" colorIndex="accent-2">
+                <PdfIcon size="large" colorIndex="light-1" />
+              </Brick>
+              <Brick label="Presentation" href="https://github.com/bryanjacquot/theme-designer-capstone/blob/master/docs/M7-JacquotBryan.pdf" colorIndex="ok">
+                <PdfIcon size="large" colorIndex="light-1" />
+              </Brick>
             </Bricks>
           </Box>
           <Footer float={true} colorIndex="grey-3-a" pad={{vertical: "small", horizontal: "large"}}>
