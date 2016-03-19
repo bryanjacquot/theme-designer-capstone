4ee3212c0350e278f5ef4b9b3f0f9ea742f0a528
Bryan Jacquot <jacquot@hpe.com>
2016-03-19 09:53:34 -0600

    Added server for Heroku and updated package file to start server
    Signed-off-by: Bryan Jacquot <jacquot@hpe.com>

---
+++ server/server.js
@@ -0,0 +1,61 @@
+// (C) Copyright 2014-2015 Hewlett-Packard Development Company, L.P.
+
+var compression = require('compression');
+var express = require('express');
+var http = require("http");
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var path = require('path');
+var sass = require('node-sass');
+
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+
+var webpackRequire = require('enhanced-require')(module, {
+  resolve: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel?presets[]=react,presets[]=es2015',
+        exclude: /(node_modules\/intl|node_modules\/moment|node_modules\/react)/
+      }
+    ]
+  }
+});
+
+var server = express();
+var App = React.createFactory(webpackRequire('../src/js/components/Home'));
+
+var theme = sass.renderSync({
+  file: 'node_modules/grommet/scss/vanilla/index.scss',
+  includePaths: [path.resolve(__dirname, '../node_modules')]
+});
+
+var PORT = process.env.PORT || 8050;
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(compression());
+
+app.use(morgan('tiny'));
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+
+  // server side rendering with react + webpack
+  //var html = ReactDOMServer.renderToString(App({}));
+  //res.render('index.ejs', {appBody: html, styleContent: '<style>' + theme.css + '</style>'});
+
+  //for single page app uncomment following line (remember to comment the 2 lines above)
+  res.sendFile(path.resolve(path.join(__dirname, '/../dist/index.html')));
+});
+
+app.use('/', express.static(path.join(__dirname, '/../dist')));
+
+var server = http.createServer(app);
+server.listen(PORT);
+
+console.log('Server started, listening at: http://localhost:8050...');
