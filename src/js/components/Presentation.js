87fadfe877c48090d6cff963259abe3e8ee7d08b
Bryan Jacquot <jacquot@hpe.com>
2016-03-26 12:30:51 -0600

    Added presentation content.
    Signed-off-by: Bryan Jacquot <jacquot@hpe.com>

--- src/js/components/Presentation.js
+++ src/js/components/Presentation.js
@@ -14,10 +14,12 @@ export default class Presentation extends Component {
 			<Section>
 			  <h2>Presentation</h2>
         <p>
-			  This presentation summarizes the Grommet Theme Designer project.  It was presented
-        for the oral defense of my M.S. in Human-Computer Interaction degree.
+			  The following presentation summarizes the Grommet Theme Designer project.
+        It was presented for the oral defense of my M.S. in Human-Computer
+        Interaction degree.  The revised mockups are also provided.
         </p>
-			  <Anchor icon={<Pdf/>} href="https://github.com/bryanjacquot/theme-designer-capstone/raw/master/docs/M6-JacquotBryan.pdf" label="Presentation Slides" primary={true} />
+			  <Anchor icon={<Pdf/>} href="https://github.com/bryanjacquot/theme-designer-capstone/raw/master/docs/M7-JacquotBryan.pdf" label="Presentation Slides" primary={true} />
+        <Anchor icon={<Pdf/>} href="https://github.com/bryanjacquot/theme-designer-capstone/raw/master/docs/M7-JacquotBryan-Mockups.pdf" label="Revised Mockups" primary={true} />
 			</Section>
     );
   }
